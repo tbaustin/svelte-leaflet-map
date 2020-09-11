ee1dbd27101a2de176337257eae250b8d1d78e26
tbaustin <tbaustin1992@gmail.com>
2020-09-11 16:02:31 -0500

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Locator from './Components/Map.svelte'
+</script>
+
+<main>
+	<h1>Dealer Map</h1>
+	<div style="height:600px;width:100%">
+		<Locator />
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
